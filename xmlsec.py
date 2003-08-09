64520fd71ef27c5d2b7e516427c7a6af4b70c340
Valéry Febvre <vfebvre@easter-eggs.com>
2003-08-09 14:06:57 +0000

    5 transforms Ids and 6 KeyData formats were added
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@17 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- xmlsec.py
+++ xmlsec.py
@@ -1,4 +1,6 @@
-# $id$
+#! /usr/bin/env python
+
+# $Id$
 #
 # pyxmlsec -- A Python binding for XML Security library (XMLSec)
 #
@@ -20,8 +22,6 @@
 # You should have received a copy of the GNU General Public License
 # along with this program; if not, write to the Free Software
 # Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA  02111-1307  USA
-
-#! /usr/bin/env python
 
 import libxml2
 import libxslt
