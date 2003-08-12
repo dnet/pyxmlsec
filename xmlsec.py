600103090634898417ed4f4d642dc2e0f572e6d8
Valéry Febvre <vfebvre@easter-eggs.com>
2003-08-12 16:09:01 +0000

    Many many changes
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@34 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- xmlsec.py
+++ xmlsec.py
@@ -39,17 +39,18 @@ def findNode(parent, name, ns):
 def shutdown():
     return xmlsecmod.shutdown()
 
-def dsigCtxCreate(keyMngr=None):
-    ret = xmlsecmod.dsigCtxCreate(keyMngr)
-    if ret is None: raise parserError('xmlSecDSigCtxCreate() failed')
-    return DSigCtx(_obj=ret)
-
 def cryptoAppInit(config=None):
     return xmlsecmod.cryptoAppInit(config)
 def cryptoAppKeyLoad(filename, format, pwd, pwdCallback, pwdCallbackCtx):
     ret = xmlsecmod.cryptoAppKeyLoad(filename, format, pwd, pwdCallback, pwdCallbackCtx)
-    if ret is None: raise parserError('xmlSecTmplSignatureAddReference() failed')
+    if ret is None: raise parserError('xmlSecCryptoAppKeyLoad() failed')
     return Key(_obj=ret)
+def cryptoAppDefaultKeysMngrInit(mngr):
+    return xmlsecmod.cryptoAppDefaultKeysMngrInit(mngr)
+def cryptoAppDefaultKeysMngrAdoptKey(mngr, key):
+    return xmlsecmod.cryptoAppDefaultKeysMngrAdoptKey(mngr, key)
+def cryptoAppKeysMngrCertLoad(mngr, filename, format, type):
+    return xmlsecmod.cryptoAppKeysMngrCertLoad(mngr, filename, format, type)
 def cryptoAppShutdown():
     return xmlsecmod.cryptoAppShutdown()
 
@@ -58,18 +59,44 @@ def cryptoInit():
 def cryptoShutdown():
     return xmlsecmod.cryptoShutdown()
 
+def dsigCtxCreate(keysMngr=None):
+    ret = xmlsecmod.dsigCtxCreate(keysMngr)
+    if ret is None: raise parserError('xmlSecDSigCtxCreate() failed')
+    return DSigCtx(_obj=ret)
+
+def ptrListCreate(id=None):
+    ret = xmlsecmod.ptrListCreate(id)
+    if ret is None: raise parserError('xmlSecPtrListCreate() failed')
+    return PtrList(_obj=ret)
+
 def tmplSignatureCreate(doc, c14nMethodId, signMethodId, id=None):
     ret = xmlsecmod.tmplSignatureCreate(doc, c14nMethodId, signMethodId, id)
     if ret is None: raise parserError('xmlSecTmplSignatureCreate() failed')
     return TmplSignature(_obj=ret)
 
-## Transforms Ids
+def keysMngrCreate():
+    ret = xmlsecmod.keysMngrCreate()
+    if ret is None: raise parserError('xmlSecKeysMngrCreate() failed')
+    return KeysMngr(_obj=ret)
+
+## Transforms Ids methods
+transformInclC14NId  = xmlsecmod.transformInclC14NId()
 transformExclC14NId  = xmlsecmod.transformExclC14NId()
 transformEnvelopedId = xmlsecmod.transformEnvelopedId()
 transformDsaSha1Id   = xmlsecmod.transformDsaSha1Id()
 transformRsaSha1Id   = xmlsecmod.transformRsaSha1Id()
 transformSha1Id      = xmlsecmod.transformSha1Id()
+## Key data ids methods
+keyDataDsaId  = xmlsecmod.keyDataDsaId()
+keyDataRsaId  = xmlsecmod.keyDataRsaId()
+keyDataX509Id = xmlsecmod.keyDataX509Id()
 
+TransformUriTypeNone         = 0x0000 # The URI type is unknown or not set.
+TransformUriTypeEmpty        = 0x0001 # The empty URI ("") type.
+TransformUriTypeSameDocument = 0x0002 # The smae document ("#...") but not empty ("") URI type.	
+TransformUriTypeLocal        = 0x0004 # The local URI ("file:///....") type.
+TransformUriTypeRemote       = 0x0008 # The remote URI type.
+TransformUriTypeAny          = 0xFFFF # Any URI type.
 
 class DSigCtx:
     def __init__(self, _obj=None):
@@ -88,11 +115,35 @@ class DSigCtx:
         return xmlsecmod.dsigCtxSign(self, tmpl)
     def verify(self, node):
         return xmlsecmod.dsigCtxVerify(self, node)
-    def setSignKey(self, signKey):
-        self._o = xmlsecmod.dsigCtxSetSignKey(self, signKey)
+    def enableReferenceTransform(self, transformId):
+        return xmlsecmod.dsigCtxEnableReferenceTransform(self, transformId)
+    def enableSignatureTransform(self, transformId):
+        return xmlsecmod.dsigCtxEnableSignatureTransform(self, transformId)
+    def setSignKey(self, key):
+        self._o = xmlsecmod.dsigCtxSetSignKey(self, key)
+    def setEnabledReferenceUris(self, value):
+        self._o = xmlsecmod.dsigCtxSetEnabledReferenceUris(self, value)
     def getStatus(self):
         return xmlsecmod.dsigCtxGetStatus(self)
+    def getKeyInfoReadCtx(self):
+        return KeyInfoCtx(_obj=xmlsecmod.dsigCtxGetKeyInfoReadCtx(self))
+    def getSignedInfoReferences(self):
+        return PtrList(_obj=xmlsecmod.dsigCtxGetSignedInfoReferences(self))
 
+class PtrList:
+    def __init__(self, _obj=None):
+	if _obj != None:
+            self._o = _obj
+            return
+        self._o = None
+    def __repr__(self):
+        return "<xmlSecPtrList object at 0x%x>" % id (self)
+    def destroy(self):
+        return xmlsecmod.ptrListDestroy(self)
+    def add(self, item):
+        return xmlsecmod.ptrListAdd(self, item)
+    def getSize(self):
+        return xmlsecmod.ptrListGetSize(self)
 
 class TmplSignature(libxml2.xmlNode):
     def __init__(self, _obj=None):
@@ -135,6 +186,16 @@ class TmplReference(libxml2.xmlNode):
         return xmlsecmod.tmplReferenceAddTransform(self, transformId)
 
 
+KeyDataTypeUnknown   = 0x0000 # The key data type is unknown (same as #xmlSecKeyDataTypeNone)
+KeyDataTypeNone	     = KeyDataTypeUnknown
+KeyDataTypePublic    = 0x0001 # The key data contain a public key.
+KeyDataTypePrivate   = 0x0002 # The key data contain a private key.
+KeyDataTypeSymmetric = 0x0004 # The key data contain a symmetric key.
+KeyDataTypeSession   = 0x0008 # The key data contain session key (one time key, not stored in keys manager).
+KeyDataTypePermanent = 0x0010 # The key data contain permanent key (stored in keys manager).
+KeyDataTypeTrusted   = 0x0100 # The key data is trusted.
+KeyDataTypeAny       = 0xFFFF # Any key data.
+
 KeyDataFormatUnknown  = 0 # the key data format is unknown.
 KeyDataFormatBinary   = 1 # the binary key data.
 KeyDataFormatPem      = 2 # the PEM key data (cert or public/private key).
@@ -149,5 +210,29 @@ class Key:
         self._o = None
     def __repr__(self):
         return "<xmlSecKey object at 0x%x>" % id (self)
+    def destroy(self):
+        xmlsecmod.keyDestroy(self)
     def setName(self, name):
         return xmlsecmod.keySetName(self, name)
+
+class KeyInfoCtx:
+    def __init__(self, _obj=None):
+	if _obj != None:
+           self._o = _obj
+           return
+        self._o = None
+    def __repr__(self):
+        return "<xmlSecKeyInfoCtx object at 0x%x>" % id (self)
+    def getEnabledKeyData(self):
+        return PtrList(_obj=xmlsecmod.getEnabledKeyData(self))
+
+class KeysMngr:
+    def __init__(self, _obj=None):
+	if _obj != None:
+           self._o = _obj
+           return
+        self._o = None
+    def __repr__(self):
+        return "<xmlSecKeysMngr object at 0x%x>" % id (self)
+    def destroy(self):
+        xmlsecmod.keysMngrDestroy(self)
