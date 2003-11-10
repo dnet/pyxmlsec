c82304db80a76a04cd6355946aa95524585660a7
Valéry Febvre <vfebvre@easter-eggs.com>
2003-11-10 02:02:41 +0000

    Struct xmlSecTransformPtr_object and #define xmlSecTransformPtr_get(v) added
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@94 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- transforms.h
+++ transforms.h
@@ -1,5 +1,11 @@
 #include <xmlsec/transforms.h>
 
+typedef struct {
+    PyObject_HEAD
+    xmlSecTransformPtr obj;
+} xmlSecTransformPtr_object;
+#define xmlSecTransformPtr_get(v) (((v) == Py_None) ? NULL : (((xmlSecTransformPtr_object *)(v))->obj))
+
 PyObject *xmlsec_TransformInclC14NId(PyObject *self, PyObject *args);
 PyObject *xmlsec_TransformExclC14NId(PyObject *self, PyObject *args);
 PyObject *xmlsec_TransformEnvelopedId(PyObject *self, PyObject *args);
