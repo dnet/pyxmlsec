1dd4419a2e1858b6009ce21157c1b7334a1d3c01
Valéry Febvre <vfebvre@easter-eggs.com>
2003-11-11 17:28:45 +0000

    Completly rewritten
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@102 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- examples/verify1.py
+++ examples/verify1.py
@@ -1,40 +1,134 @@
 #!/usr/bin/env python
+#
+# $Id$
+#
+# PyXMLSec example: Verifying a file using a single key.
+#
+# Verifies a file using a key from PEM file.
+#
+# Usage: 
+#	./verify1.py <signed-file> <pem-key> 
+#
+# Example:
+#	./verify1.py sign1-res.xml rsapub.pem
+#	./verify1.py sign2-res.xml rsapub.pem
+#
+# This is free software; see COPYING file in the source
+# distribution for preciese wording.
+# 
+# Copyright (C) 2002-2003 Aleksey Sanin <aleksey@aleksey.com>
+# Copyright (C) 2003 Valery Febvre <vfebvre@easter-eggs.com>
+#
 
 import sys
 sys.path.insert(0, '../')
 
 import libxml2
-import libxslt
-
 import xmlsec
 
-libxml2.initParser()
-libxml2.substituteEntitiesDefault(1)
+def main():
+    assert(sys.argv)
+    if len(sys.argv) < 3:
+        print "Error: wrong number of arguments."
+        print "Usage: %s <xml-file> <key-file>" % sys.argv[0]
+        return sys.exit(1)
+    
+    # Init libxml library
+    libxml2.initParser()
+    libxml2.substituteEntitiesDefault(1)
 
-xmlsec.init()
+    # Init xmlsec library
+    if xmlsec.init() < 0:
+        print "Error: xmlsec initialization failed."
+        return sys.exit(-1)
+    
+    # Check loaded library version
+    if xmlsec.checkVersion() != 1:
+	print "Error: loaded xmlsec library version is not compatible.\n"
+	sys.exit(-1)
 
-xmlsec.cryptoAppInit(None)
-xmlsec.cryptoInit()
+    # Init crypto library
+    if xmlsec.cryptoAppInit(None) < 0:
+        print "Error: crypto initialization failed."
+    
+    # Init xmlsec-crypto library
+    if xmlsec.cryptoInit() < 0:
+        print "Error: xmlsec-crypto initialization failed."
 
-doc = libxml2.parseFile("./sign1-res.xml")
-node = xmlsec.findNode(doc.getRootElement(), xmlsec.NodeSignature, xmlsec.DSigNs)
-dsig_ctx = xmlsec.DSigCtx()
-key = xmlsec.cryptoAppKeyLoad("./rsapub.pem", xmlsec.KeyDataFormatPem, None, None, None)
-dsig_ctx.setSignKey(key)
-key.setName("./rsapub.pem")
-dsig_ctx.verify(node)
+    res = verify_file(sys.argv[1], sys.argv[2])
 
-if dsig_ctx.getStatus():
-    print "Signature is OK"
-else:
-    print "Signature is INVALID"
+    # Shutdown xmlsec-crypto library
+    xmlsec.cryptoShutdown()
 
-dsig_ctx.destroy()
-doc.freeDoc()
+    # Shutdown crypto library
+    xmlsec.cryptoAppShutdown()
 
-xmlsec.cryptoShutdown()
-xmlsec.cryptoAppShutdown()
+    # Shutdown xmlsec library
+    xmlsec.shutdown()
 
-xmlsec.shutdown()
+    # Shutdown LibXML2
+    libxml2.cleanupParser()
 
-libxml2.cleanupParser()
+    sys.exit(res)
+
+
+# Verifies XML signature in xml_file using public key from key_file.
+# Returns 0 on success or a negative value if an error occurs.
+def verify_file(xml_file, key_file):
+    assert(xml_file)
+    assert(key_file)
+
+    # Load XML file
+    doc = libxml2.parseFile(xml_file)
+    if doc is None or doc.getRootElement() is None:
+	print "Error: unable to parse file \"%s\"" % tmpl_file
+        return cleanup(doc)
+
+    # Find start node
+    node = xmlsec.findNode(doc.getRootElement(),
+                           xmlsec.NodeSignature, xmlsec.DSigNs)
+
+    # Create signature context, we don't need keys manager in this example
+    dsig_ctx = xmlsec.DSigCtx()
+    if dsig_ctx is None:
+        print "Error: failed to create signature context"
+        return cleanup(doc, dsig_ctx)
+
+    # Load private key, assuming that there is not password
+    key = xmlsec.cryptoAppKeyLoad("./rsapub.pem", xmlsec.KeyDataFormatPem,
+                                  None, None, None)
+    if key is None:
+        print "Error: failed to load private pem key from \"%s\"" % key_file
+        return cleanup(doc, dsig_ctx)
+    dsig_ctx.setSignKey(key)
+
+    # Set key name to the file name, this is just an example!
+    if key.setName(key_file) < 0:
+        print "Error: failed to set key name for key from \"%s\"" % key_file
+        return cleanup(doc, dsig_ctx)
+
+    # Verify signature
+    if dsig_ctx.verify(node) < 0:
+        print "Error: signature verify"
+        return cleanup(doc, dsig_ctx)
+
+    # Print verification result to stdout
+    if dsig_ctx.getStatus() == xmlsec.DSigStatusSucceeded:
+        print "Signature is OK"
+    else:
+        print "Signature is INVALID"
+
+    # Success
+    return cleanup(doc, dsig_ctx, 1)
+
+
+def cleanup(doc=None, dsig_ctx=None, res=-1):
+    if dsig_ctx is not None:
+        dsig_ctx.destroy()
+    if doc is not None:
+        doc.freeDoc()
+    return res
+
+
+if __name__ == "__main__":
+    main()
