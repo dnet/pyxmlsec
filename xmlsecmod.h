3867b6324d63ba30ff6b0a74045552a34a2b5a41
Valéry Febvre <vfebvre@easter-eggs.com>
2003-11-13 01:25:44 +0000

    xmlOutputBufferPtr_object struct and #define xmlOutputBufferPtr_get() added
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@115 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- xmlsecmod.h
+++ xmlsecmod.h
@@ -11,7 +11,13 @@ typedef struct {
     xmlNodePtr obj;
 } xmlNodePtr_object;
 
+typedef struct {
+    PyObject_HEAD
+    xmlOutputBufferPtr obj;
+} xmlOutputBufferPtr_object;
+
 #define xmlDocPtr_get(v) (((v) == Py_None) ? NULL : (((xmlDocPtr_object *)(v))->obj))
 #define xmlNodePtr_get(v) (((v) == Py_None) ? NULL : (((xmlNodePtr_object *)(v))->obj))
+#define xmlOutputBufferPtr_get(v) (((v) == Py_None) ? NULL : (((xmlOutputBufferPtr_object *)(v))->obj))
 
 PyObject *xmlsec_error;
