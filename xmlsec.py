8a625f5182f675ad566b56563b0a8930b56d67cd
Valéry Febvre <vfebvre@easter-eggs.com>
2005-12-12 15:32:50 +0000

    -
    git-svn-id: svn://labs.libre-entreprise.org/svnroot/pyxmlsec/trunk@360 5620b44f-bb07-0410-b6ee-9267a7af8f61

--- xmlsec.py
+++ xmlsec.py
@@ -8,7 +8,7 @@
 # Copyright (C) 2003-2005 Easter-eggs, Valéry Febvre
 # http://pyxmlsec.labs.libre-entreprise.org
 #
-# Author: Valéry Febvre <vfebvre@easter-eggs.com>
+# Author: Valery Febvre <vfebvre@easter-eggs.com>
 #
 # This program is free software; you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
